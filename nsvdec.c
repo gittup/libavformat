gittup/libavformat

commit 861bf71c7bc63e86215857ab4afd6ac42f5f1b18

The NSV demuxer assumes that a video frame's timestamp increases by one on each frame, but some low-bitrate NSV files omit video frames for some NSV frames, and expect the timestamp to increase by one every NSV frame. This is noticeable in 64vp3.nsv where the video runs several times faster than the audio. Fix this by unconditionally incrementing the video's timestamp with each NSV frame. patch by David Conrad, umovimus gmail com
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavformat@8466 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

nsvdec.c
@@ -602,13 +602,15 @@ null_chunk_retry:
         pkt = &nsv->ahead[NSV_ST_VIDEO];
         av_get_packet(pb, pkt, vsize);
         pkt->stream_index = st[NSV_ST_VIDEO]->index;//NSV_ST_VIDEO;
-        pkt->dts = nst->frame_offset++;
+        pkt->dts = nst->frame_offset;
         pkt->flags |= nsv->state == NSV_HAS_READ_NSVS ? PKT_FLAG_KEY : 0; /* keyframe only likely on a sync frame */
 /*
         for (i = 0; i < MIN(8, vsize); i++)
             PRINT(("NSV video: [%d] = %02x\n", i, pkt->data[i]));
 */
     }
+    ((NSVStream*)st[NSV_ST_VIDEO]->priv_data)->frame_offset++;
+
     if (asize/*st[NSV_ST_AUDIO]*/) {
         nst = st[NSV_ST_AUDIO]->priv_data;
         pkt = &nsv->ahead[NSV_ST_AUDIO];

commit 180eea8f94e853b37ab7d66b41dea29d378b40ea

fix segfault with http://samples.mplayerhq.hu/nsv/nsv_samples/ot171_novideo.nsv.bz2 bug introduced in r8466 closes issue179
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavformat@10544 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

nsvdec.c
@@ -609,6 +609,7 @@ null_chunk_retry:
             PRINT(("NSV video: [%d] = %02x\n", i, pkt->data[i]));
 */
     }
+    if(st[NSV_ST_VIDEO])
     ((NSVStream*)st[NSV_ST_VIDEO]->priv_data)->frame_offset++;
 
     if (asize/*st[NSV_ST_AUDIO]*/) {