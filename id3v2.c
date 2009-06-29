gittup/libavformat

commit 2bba6d08eac658314f6f49ed146ed129472c241c

Fix id3v2.3/4 tag size parsing. Fixes issue1106
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavformat@19275 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

id3v2.c
@@ -155,7 +155,7 @@ void ff_id3v2_parse(AVFormatContext *s, int len, uint8_t version, uint8_t flags)
     while (len >= taghdrlen) {
         if (isv34) {
             tag  = get_be32(s->pb);
-            tlen = get_size(s->pb, 4);
+            tlen = get_be32(s->pb);
             get_be16(s->pb); /* flags */
         } else {
             tag  = get_be24(s->pb);

commit 082d84ae08bcf5fb3dc2fc12b0f3ba6306c7e25b

id3v2.4.0 uses syncsafe integers for sizes of frames. Fixes issue1234 Fixes regression introduced in r19275
git-svn-id: svn://svn.ffmpeg.org/ffmpeg/trunk/libavformat@19291 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

id3v2.c
@@ -155,7 +155,10 @@ void ff_id3v2_parse(AVFormatContext *s, int len, uint8_t version, uint8_t flags)
     while (len >= taghdrlen) {
         if (isv34) {
             tag  = get_be32(s->pb);
+            if(version==3){
             tlen = get_be32(s->pb);
+            }else
+                tlen = get_size(s->pb, 4);
             get_be16(s->pb); /* flags */
         } else {
             tag  = get_be24(s->pb);